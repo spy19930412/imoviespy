bd10bfa2e85d7a6fb6b42d9a7d90639f469b4fed
spy19930412 <812057520@qq.com>
2016-05-18 17:15:34 +0800

    1
    123

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+
+	})
+	
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-nodemon')
+    grunt.loadNpmTasks('grunt-concurrent')
+
+    grunt.option('force',true)
+    grunt.registerTask('default',['concurrent'])
+}
